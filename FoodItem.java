7d8ee51d9fecafcc82768049507add0366371fec
Krugw <Krugw@mail.gvsu.edu>
2016-11-29 19:21:48 -0500

    Add files via upload

---
+++ FoodItem.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Dave on 11/29/2016.
+ */
+public class FoodItem {
+    private String name;
+    private int size;
+
+}
---
+++ Fridge.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Dave on 11/29/2016.
+ */
+public class Fridge {
+}
